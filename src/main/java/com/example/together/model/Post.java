4783156d3c37622a8ea640e603d37826249f8add
Seungchan Back <bsc980504@outlook.com>
2022-07-22 18:40:18 +0900

    add models

---
+++ src/main/java/com/example/together/model/Post.java
@@ -0,0 +1,64 @@
+package com.example.together.model;
+
+import com.example.together.TimeStamped;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Setter
+@Getter // get 함수를 일괄적으로 만들어줍니다.
+@NoArgsConstructor // 기본 생성자를 만들어줍니다.
+@Entity // DB 테이블 역할을 합니다.
+public class Post extends TimeStamped {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private CategoryEnum category;
+
+    @Column(nullable = false)
+    private Date deadline;
+
+    @Column(nullable = false)
+    private int numberPeople;
+
+    @Column(nullable = false)
+    private int currentNumberPeople;
+
+    @Column(nullable = false)
+    private String contactMethod;
+
+    @ManyToOne
+    @JoinColumn(name = "USER_ID", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+    @Column(nullable = false)
+    private int viewCount;
+
+    public Post(String title, String content, CategoryEnum category, Date deadline, int numberPeople, String contactMethod, User user, String imageUrl) {
+        this.title = title;
+        this.content = content;
+        this.category = category;
+        this.deadline = deadline;
+        this.numberPeople = numberPeople;
+        this.currentNumberPeople = 1;
+        this.contactMethod = contactMethod;
+        this.user = user;
+        this.imageUrl = imageUrl;
+        this.viewCount = 0;
+    }
+}
--- src/main/java/com/example/together/repository/PostRepository.java
+++ src/main/java/com/example/together/repository/PostRepository.java
@@ -1,8 +1,9 @@
 package com.example.together.repository;
 
+import com.example.together.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PostRepository extends JpaRepository {
+public interface PostRepository extends JpaRepository<Post, Long> {
 }
