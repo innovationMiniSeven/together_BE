9665a2e24037ff204839258c95f5ea7d8684f5e5
yougeun6021 <dbrms1a1a@naver.com>
2022-07-25 17:33:40 +0900

    pagination 구현

--- src/main/java/com/example/together/repository/PostRepositoryCustom.java
+++ src/main/java/com/example/together/repository/PostRepositoryCustom.java
@@ -1,9 +1,11 @@
 package com.example.together.repository;
 
 import com.example.together.model.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface PostRepositoryCustom {
-    List<Post> findAllByCategoryOrderBySort(String sort, String category);
+    Page<Post> findAllByCategoryOrderBySort(String sort, String category, Pageable pageable);
 }
