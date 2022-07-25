9fd8ba23d4500b06a9130f3a0012b9cc8d403a18
dongil618 <rlaehddlf618@gmail.com>
2022-07-25 15:54:01 +0900

    Revert "getPosts 구현 - default부분, almost부분 못했음."
    This reverts commit 5f8829a48c1891b9e26f617a89dbfb4ef839cdd3.

--- src/main/java/com/example/together/model/Post.java
+++ src/main/java/com/example/together/model/Post.java
@@ -3,7 +3,9 @@ package com.example.together.model;
 import com.example.together.TimeStamped;
 import com.example.together.dto.EditPostRequestDto;
 import com.example.together.dto.PostRequestDto;
-import lombok.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.Date;
@@ -12,8 +14,6 @@ import java.util.Date;
 @Getter // get 함수를 일괄적으로 만들어줍니다.
 @NoArgsConstructor // 기본 생성자를 만들어줍니다.
 @Entity // DB 테이블 역할을 합니다.
-@AllArgsConstructor
-@ToString
 public class Post extends TimeStamped {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
